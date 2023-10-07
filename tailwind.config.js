e2b867b91fc855d998eb123278bf693f6e7636fb
rulanugrh <nextarituenak01@gmail.com>
2023-10-07 22:04:53 +0700

    feat: pus all files

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '16px'
+      },
+
+      fontFamily: {
+        firaCode: "'Fira Code', monospace",
+        rubikMono: "'Rubik Mono One', sans-serif"
+      },
+
+      colors: {
+        primaryColor: "#552E27",
+        secondaryColor: "#EEC39A",
+        thirdColor: "#D9A066",
+        darkColor: "#DF7126"
+      },
+    },
+  },
+  plugins: [],
+}
